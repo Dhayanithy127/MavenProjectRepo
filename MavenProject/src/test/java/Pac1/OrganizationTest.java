e4efefab26db4d55b9d0e4a06750e03275723322
Dhayanithy Selvarasu <dhayanithy.127@gmail.com>
2025-04-04 13:17:13 +0530

    First push

---
+++ MavenProject/src/test/java/Pac1/OrganizationTest.java
@@ -0,0 +1,26 @@
+package Pac1;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class OrganizationTest 
+{
+
+	@Test
+	public void createOrgTest()
+	{
+		System.out.println("Org created");
+	}
+	
+	@Test
+	public void modifyOrgTest()
+	{
+		System.out.println("Org Modified");
+	}
+	
+}
---
+++ MavenProject/src/test/java/Pac2/ContactTest.java
@@ -0,0 +1,24 @@
+package Pac2;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class ContactTest 
+{ 
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("Contact created");
+	}
+	
+	@Test
+	public void modifyContactTest()
+	{
+		System.out.println("Contact modified");
+	}
+}
